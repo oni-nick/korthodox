b12892b35044e783489f22162246d498ba5b5237
Nick <nicholas00105@gmail.com>
2023-05-11 01:41:53 +0900

    날짜 수정 (광고 상세, 입출금내역)

--- src/Ads/detail/index.tsx
+++ src/Ads/detail/index.tsx
@@ -34,7 +34,7 @@ function Detail(){
 
       function dateConvert(date : Date){
         const Year = date.getFullYear()
-        const Month = date.getMonth()
+        const Month = date.getMonth()+1
         const Day = date.getDate()
         const Date = Year  + '년 0' + Month + '월 ' + Day + '일';
         return(Date);
--- src/Dw/DWTableData.tsx
+++ src/Dw/DWTableData.tsx
@@ -10,7 +10,7 @@ interface DwHistory {
 
 function dateConvert(date : Date){
   const Year = date.getFullYear()
-  const Month = date.getMonth()
+  const Month = date.getMonth()+1
   const Day = date.getDate()
   const Date = Year  + '년 0' + Month + '월 ' + Day + '일';
   return(Date);

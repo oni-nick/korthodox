58c550218aaf23d8b7e31edae838ff86924b7f93
Lurang <lurangjh@naver.com>
2023-05-23 20:42:08 +0900

    dw history format 일부변경

--- src/Dw/DWTableData.tsx
+++ src/Dw/DWTableData.tsx
@@ -13,7 +13,7 @@ function dateConvert(date : Date){
   const Year = date.getFullYear()
   const Month = date.getMonth()+1
   const Day = date.getDate()
-  const Date = Year  + '년 0' + Month + '월 ' + Day + '일';
+  const Date = Year  + '년 ' + Month + '월 ' + Day + '일';
   return(Date);
 }
 
@@ -23,43 +23,42 @@ function typeChange(text : any){
   else if (text == 'admin_mint'){ return <p>관리자</p>; }
   else if (text.startsWith('ads_result')) {
       const ads = text.split('-');
-      return <Link to={`/ads/${ads[1]}`}> 광고 {ads[1]} </Link>;
+      return <Link to={`/ads/${ads[1]}`}> 광고 {ads[1]} 보상 </Link>;
   }
 
   else { return <p>없음</p>; }
 }
 
-
-  export const columns: ColumnsType<DwHistory> = [
-    {
-      align : 'center',
-      title: '날짜',
-      dataIndex: 'timestamp',
-      sorter: (a, b) =>b.timestamp - a.timestamp,
-      render: (text) => <p>{dateConvert(new Date(text))}</p>,
-    },
-    {
-      align : 'center',
-      title: '금액',
-      dataIndex: 'amount',
-      render: (text) => <p>{new BigNumber(text).toFormat()}</p>
-    },
-    {
-      align : 'center',
-      title: '종류',
-      dataIndex: '',
-      render: () => <p>ADS</p>,
-    },
-    {
-      align : 'center',
-      title: '타입',
-      dataIndex: 'type',
-      render: (text) => <p>{typeChange(text)}</p>,
-    },
-    {
-      align : 'center',
-      title: '해시',
-      dataIndex: 'hash',
-      render: (text) => <a href={`https://sepolia.etherscan.io/tx/${text}`} target='_blank'>{text.slice(0, 12)}</a>,
-    },
-  ];
+export const columns: ColumnsType<DwHistory> = [
+  {
+    align : 'center',
+    title: '날짜',
+    dataIndex: 'timestamp',
+    sorter: (a, b) =>b.timestamp - a.timestamp,
+    render: (text) => <p>{dateConvert(new Date(text))}</p>,
+  },
+  {
+    align : 'center',
+    title: '금액',
+    dataIndex: 'amount',
+    render: (text) => <p>{new BigNumber(text).toFormat()}</p>
+  },
+  {
+    align : 'center',
+    title: '종류',
+    dataIndex: '',
+    render: () => <p>ADS</p>,
+  },
+  {
+    align : 'center',
+    title: '타입',
+    dataIndex: 'type',
+    render: (text) => <p>{typeChange(text)}</p>,
+  },
+  {
+    align : 'center',
+    title: '해시',
+    dataIndex: 'hash',
+    render: (text) => <a href={`https://sepolia.etherscan.io/tx/${text}`} target='_blank'>{text.slice(0, 12)}</a>,
+  },
+];

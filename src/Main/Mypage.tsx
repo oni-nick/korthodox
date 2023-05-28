7b84861bbc5b38c6154f287d00775f5dfeb571d4
Nick <nicholas00105@gmail.com>
2023-05-29 00:29:45 +0900

    이용권 API 동작 확인, 날짜 갱신 에러

--- src/Main/Mypage.tsx
+++ src/Main/Mypage.tsx
@@ -1,33 +1,28 @@
 import { Descriptions, Table } from 'antd';
 import { useUserState, UserInfo } from '../context/user';
 import { Div, MypageDiv } from './Styles';
+import { dateConverter, UserBalance } from '../Utils';
+import axios from 'axios';
+import { useState } from 'react';
 
 function Mypage(){
     const user = useUserState();
+    
+    const [balance, setBalance] = useState<UserBalance[]>([]);
 
-    function dateConvert(date : Date){
-        const Year = date.getFullYear()
-        const Month = date.getMonth()+1
-        const Day = date.getDate()
-        const Date = Year  + '년 0' + Month + '월 ' + Day + '일';
-        return(Date);
-      }
-
-    interface UserInfo {
-        email: string;
-        level: string,
-        address: string,
-        expireDate : Date,
-        join_time: Date,
-    };
-
+    axios.get<UserBalance[]>('/api/user/balance')
+    .then(res => {
+        if(res.data){
+            setBalance(res.data)
+        }
+    })
 
     return(
         <MypageDiv>
             <Descriptions bordered size="default">    
                 <Descriptions.Item label="유저 이름"> {user.email}</Descriptions.Item>
-                <Descriptions.Item label="가입 일자">{dateConvert(new Date(user.join_time))}</Descriptions.Item>
-                <Descriptions.Item label="이용권 만료 기간">{dateConvert(new Date(user.expireDate))}</Descriptions.Item>
+                <Descriptions.Item label="가입 일자">{dateConverter(new Date(user.join_time))}</Descriptions.Item>
+                <Descriptions.Item label="이용권 만료 기간">{dateConverter(new Date(user.expireDate))}</Descriptions.Item>
                 <Descriptions.Item label="개인 지갑 주소"> {user.address}</Descriptions.Item>
             </Descriptions>
         </MypageDiv>
--- src/Utils/index.tsx
+++ src/Utils/index.tsx
@@ -1,1 +1,17 @@
 export {};
+
+export function dateConverter(date : Date){
+    const Year = date.getFullYear()
+    const Month = date.getMonth()+1
+    const Day = date.getDate()
+    const Date = Year  + '년 ' + Month + '월 ' + Day + '일';
+    return(Date);
+  }
+
+export interface UserBalance {
+  id: string,
+  user_email: string,
+  type: 'ADS' | 'ETH' | 'KRW',
+  amount: string
+  available: string
+}

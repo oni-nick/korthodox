661fc9b353179f1bc83be4b26ede480a575b1baf
Nick <nicholas00105@gmail.com>
2023-05-29 00:28:00 +0900

    이용권 구매 API 동작 확인, Context 로 정의된 유저정보 갱신 이슈 발생, reat-redux 설치

--- src/Ticket/Purchase.tsx
+++ src/Ticket/Purchase.tsx
@@ -4,19 +4,16 @@ import { Button, Checkbox, Descriptions } from "antd";
 import BigNumber from 'bignumber.js';
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { UserInfo, useUserDispatch } from '../context/user'
+import { UserInfo, useUserDispatch, useUserState } from '../context/user'
+import { dateConverter, UserBalance } from "../Utils";
+import { useSelector } from 'react-redux';
 
-interface UserBalance {
-    id: string,
-    user_email: string,
-    type: 'ADS' | 'ETH' | 'KRW',
-    amount: string
-    available: string
-  }
 
 function Purchase(){    
     const url = useLocation();
-    const dispatch = useUserDispatch()
+    const user = useUserState();
+    // const user_new = useSelector(state : RootState => state.data);
+    const dispatch = useUserDispatch();
     const [searchParams, setSearchParams]=useSearchParams();
     const days = searchParams.get('days');
     const be_price = searchParams.get('price');
@@ -24,29 +21,30 @@ function Purchase(){
     
     const [balance, setBalance] = useState<UserBalance[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
-    
+    // Console Log
     console.log(balance);
+    console.log(user.expireDate);
 
     const PurchaseClick = async () => {
-        setLoading(true);
-        // await
-        // axios.post<UserInfo>('/api/user/buyticket', days)
-        // .then(res => {
-        //     if (res.data){
-        //         return dispatch({type: 'LOG_IN', data: res.data})
-        //     }
-        // });
-        axios.post('/api/user/buyticket', days);
-        axios.get<UserBalance[] | null>('/api/user/balance')
-        .then(res => {
-          if (res.data) {
-            setBalance(res.data)
-          }
-        });
-
-        setTimeout(()=>{setLoading(false)});
+        try {
+            setLoading(true);
+            const result = await axios.post('/api/user/buyticket', {day : Number(days)});
+            dispatch({type: 'LOG_IN', data: result.data});
+            
+            // Console Log
+            console.log('post response user data:', result!);
+            
+            const res = await axios.get<UserBalance[] | null>('/api/user/balance');
+            if (res.data) {
+                setBalance(res.data)
+            }
+        } catch(error) {
+            console.log(error);
+            
+        } finally {
+            setLoading(false);
+        }
     };
-
     // useEffect(() => {
     //     axios.post<UserInfo>('/api/user/buyticket', days)
     //     .then(res => {
@@ -66,6 +64,8 @@ function Purchase(){
                 {days}일간 AdsRider 서비스를 이용할 수 있는 티켓입니다.
                 </Descriptions.Item>
             </Descriptions>
+            <p>이용권 기간: {dateConverter(new Date(user.expireDate))}</p>
+            <p>잔액: {balance[2]?.type} {balance[2]?.amount}</p>
             <div>
                 <P>※주의사항※<br/>구매일 기준 이용권 날짜가 갱신됩니다.<br/>ADS 코인으로 결제됩니다.</P>
                 <Checkbox style={{margin : '10px 10px 30px 20px'}}>주의사항을 확인했습니다.</Checkbox>

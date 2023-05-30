ec1a4e136406a35df5f3a76b654bbf817ad572bc
Lurang <lurangjh@naver.com>
2023-05-30 13:41:02 +0900

    fail, success 페이지 추가

---
+++ src/Coin/Success.tsx
@@ -0,0 +1,13 @@
+import { useSearchParams } from 'react-router-dom';
+
+export function SuccessPage() {
+    const [searchParams] = useSearchParams();
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
